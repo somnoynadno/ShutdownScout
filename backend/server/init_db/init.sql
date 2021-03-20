2a63963b6d8650ca639758516423c711889c0772
amoniaka-knabino <sv@dynfor.ru>
2021-03-20 22:58:45 +0500

    timestamp is not unique

--- backend/server/init_db/init.sql
+++ backend/server/init_db/init.sql
@@ -3,7 +3,7 @@ country_name varchar not null unique,
 top_sites varchar not null);
 
 CREATE TABLE IF NOT EXISTS public.PingRecord(
-timestamp timestamp NOT NULL DEFAULT NOW() unique,
+timestamp timestamp NOT NULL DEFAULT NOW(),
 user_ip varchar not null,
 user_region varchar not null,
 pinged_county varchar not null,
