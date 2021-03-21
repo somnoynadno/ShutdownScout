d020511a557fa3e9850b744959fe24c02b7e1fd7
amoniaka-knabino <sv@dynfor.ru>
2021-03-21 15:23:06 +0500

    remove multithreading from proxy tester

--- backend/server/proxy_tester.py
+++ backend/server/proxy_tester.py
@@ -3,16 +3,9 @@ from sys import argv
 import datetime
 import requests
 
-import threading
-import resource
-from itertools import zip_longest
-
-ans_lock = threading.Lock()
-ans = {}
-
 def ping_site(url, proxy):
     try:
-        resp = requests.get(f"http://{url}", proxies=proxy, timeout=5)
+        resp = requests.get(f"http://{url}", proxies=proxy, timeout=3)
         if resp.ok:
             return 1
         return 0
@@ -20,7 +13,16 @@ def ping_site(url, proxy):
         return 0
     return 0
 
-def ping_country(country, sites_list):
+proxy_url = argv[1]
+
+with open('init_db/web_pool.json', "r") as f:
+    top_sites = json.load(f)
+
+ans = {}
+proxies = {"http":proxy_url}
+
+for country in top_sites:
+    sites_list = top_sites[country]
     availability=0
     ping = 0
     for site in sites_list:
@@ -30,27 +32,10 @@ def ping_country(country, sites_list):
             availability += ping_site(site, proxies)
             time_delta = datetime.datetime.now() - ts
             ping += time_delta.total_seconds()*10**3
-    country_av = availability / len(sites_list)
+    country_av = availability // len(sites_list)
     country_ping = ping // len(sites_list)
-    global ans
-    with ans_lock:
-        ans[country] = {"Ping":country_ping, "Availability":country_av}
+    ans[country] = {"Ping":country_ping, "Availability":country_av}
     print(country, ans[country])
-
-proxy_url = argv[1]
-
-with open('init_db/web_pool.json', "r") as f:
-    top_sites = json.load(f)
-
-ans = {}
-proxies = {"http":proxy_url}
-threads = []
-for country in top_sites:
-    thread = threading.Thread(target=ping_country, args=(country, top_sites[country]))
-    threads.append(thread)
-    thread.start()
-for thread in threads:
-    thread.join()
 
 with open('test.json', 'w') as f:
     json.dump(ans, f)
