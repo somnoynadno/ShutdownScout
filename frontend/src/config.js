b0c4996040440a5c31c910200e24822317d2ffdf
Alexander Zorkin <somnoynadno@yandex.ru>
2021-06-27 15:01:03 +0700

    Typo fixed

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -4,7 +4,7 @@ export const mapColors = {
     "green": "#4caf50",
 }
 
-export const isLocal = process.env.HOST_IP_ADDRESS === "LOCAL";
+export const isLocal = process.env.REACT_APP_HOST_ENV === "LOCAL";
 export const apiAddress = isLocal ? 'http://localhost:3113/api' : "http://scout.somnoynadno.ru/api";
 
 export const maxPingListSize = (process.env.NODE_ENV === "production" ? 500 : 10);
