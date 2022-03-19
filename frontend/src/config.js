554ab715f4c245cf0190004eec5f61223780a7e6
Alexander Zorkin <46386987+somnoynadno@users.noreply.github.com>
2022-03-19 15:52:45 +0000

    HTTP -> HTTPS

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -5,6 +5,6 @@ export const mapColors = {
 }
 
 export const isLocal = process.env.REACT_APP_HOST_ENV === "LOCAL";
-export const apiAddress = isLocal ? 'http://localhost:3113/api' : "http://scout.somnoynadno.ru/api";
+export const apiAddress = isLocal ? 'http://localhost:3113/api' : "https://scout.somnoynadno.ru/api";
 
 export const maxPingListSize = (process.env.NODE_ENV === "production" ? 500 : 10);
